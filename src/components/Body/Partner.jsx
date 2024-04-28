ee5b1c6057bf40a728d620f031144f859a5782b7
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-04-28 23:02:40 +0600

    Footer Done

--- src/components/Body/Partner.jsx
+++ src/components/Body/Partner.jsx
@@ -3,8 +3,8 @@ import Marquee from "react-fast-marquee";
 const Partner = () => {
   return (
     <>
-      <h2 className='my-5 md:my-10 text-center font-bold text-4xl'>Our Partners</h2>
-      <div className='w-full h-40 flex items-center justify-center bg-gray-200'>
+      <h2 className='my-5 md:my-10 text-center font-bold text-2xl md:text-4xl'>Our Partners</h2>
+      <div className='w-full h-40 flex items-center justify-center shadow-2xl'>
         <Marquee speed={70}>
           <img
             className='mr-10 h-28'
