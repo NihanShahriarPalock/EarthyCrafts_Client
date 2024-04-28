4dc46bb741bf4a6e0b7f9f579960da325110a34f
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-04-29 00:35:30 +0600

    Add New Item Design Changed

--- src/pages/AddCraft.jsx
+++ src/pages/AddCraft.jsx
@@ -1,7 +1,6 @@
 // import { toast } from "react-toastify";
 import UseAuth from "../hooks/UseAuth";
-import { toast,ToastBar } from "react-hot-toast";
-
+import { toast, ToastBar } from "react-hot-toast";
 
 const AddCraft = () => {
   const { user } = UseAuth() || {};
@@ -46,99 +45,180 @@ const AddCraft = () => {
       .then((data) => {
         console.log(data);
         if (data?.insertedId) {
-           toast.success(`${item_name} is inserted`);
+          toast.success(`${item_name} is inserted`);
         }
         form.reset();
       });
   };
   return (
-    <div>
-      <h2>Add Craft</h2>
-      <form onSubmit={handleAddCraft}>
-        <div>
-          Image url
+    <div className='w-full lg:w-3/4  mx-auto'>
+      <h2 className='my-5 text-center font-bold text-2xl md:text-4xl '>
+        Add A New Item
+      </h2>
+      <form
+        onSubmit={handleAddCraft}
+        className='grid grid-cols-1 md:grid-cols-2 gap-3  shadow-lg p-2 lg:p-6 border-2 border-gray-200'>
+        <div className='flex flex-col gap-1'>
+          <label className='font-semibold'>
+            Image url <span className='text-red-500'>&#42;</span>
+          </label>
           <input
-            type='text'
-            placeholder='Image URL'
-            className='input input-bordered '
+            className='border-[1px] border-white   bg-gray-300 p-2 rounded-md'
+            placeholder='Enter Image URL'
+            required
             name='image'
-          />
-        </div>
-        <div>
-          item_name
-          <input
             type='text'
-            placeholder='Enter item name'
-            className='input input-bordered '
-            name='item_name'
-          />
-        </div>
-        <div>
-          subcategory_Name
-          <input
-            type='text'
-            placeholder='Enter subcategory Name'
-            className='input input-bordered '
-            name='subcategory_Name'
-          />
-        </div>
-        <div>
-          short description
-          <input
-            type='text'
-            placeholder='short description'
-            className='input input-bordered '
-            name='short_description'
-          />
-        </div>
-        <div>
-          price
-          <input
-            type='text'
-            placeholder='price'
-            className='input input-bordered '
-            name='price'
-          />
-        </div>
-        <div>
-          rating
-          <input
-            type='text'
-            placeholder='rating'
-            className='input input-bordered '
-            name='rating'
-          />
-        </div>
-        <div>
-          customization
-          <input
-            type='text'
-            placeholder='customization-'
-            className='input input-bordered '
-            name='customization'
-          />
-        </div>
-        <div>
-          processing_time
-          <input
-            type='text'
-            placeholder='processing_time'
-            className='input input-bordered '
-            name='processing_time'
-          />
-        </div>
-        <div>
-          stockStatus
-          <input
-            type='text'
-            placeholder='stockStatus'
-            className='input input-bordered '
-            name='stockStatus'
           />
         </div>
 
-        <div>
-          <input className='btn btn-primary' type='submit' value='Submit' />
+        <div className='flex flex-col gap-1'>
+          <label className='font-semibold'>
+            Item Name <span className='text-red-500'>&#42;</span>
+          </label>
+          <input
+            className='border-[1px] border-white   bg-gray-300 p-2 rounded-md'
+            placeholder='Enter Item Name'
+            required
+            name='item_name'
+            type='text'
+          />
+        </div>
+
+        <div className='flex flex-col gap-1'>
+          <label className='font-semibold'>
+            Subcategory Name <span className='text-red-500'>&#42;</span>
+          </label>
+          <select
+            className='border-[1px] border-white bg-gray-300 p-2 rounded-md'
+            required
+            name='subcategory_Name'>
+            <option value=''>Select Subcategory</option>
+            <option value='Wooden Furniture & Sculptures'>
+              Wooden Furniture & Sculptures
+            </option>
+            <option value='Wooden Home Decor'>Wooden Home Decor</option>
+            <option value='Wooden Utensils and Kitchenware'>
+              Wooden Utensils and Kitchenware
+            </option>
+            <option value='Jute Home Decor'>Jute Home Decor</option>
+            <option value='Jute Kitchenware & utensils'>
+              Jute Kitchenware & utensils
+            </option>
+            <option value='Jute and wooden jewellery'>
+              Jute and wooden jewellery
+            </option>
+          </select>
+        </div>
+
+        <div className='flex flex-col gap-1'>
+          <label className='font-semibold'>
+            Price <span className='text-red-500'>&#42;</span>
+          </label>
+          <input
+            className='border-[1px] border-white   bg-gray-300 p-2 rounded-md'
+            placeholder='Enter Price'
+            required
+            name='price'
+            type='text'
+          />
+        </div>
+
+        <div className='flex flex-col gap-1'>
+          <label className='font-semibold'>
+            Rating <span className='text-red-500'>&#42;</span>
+          </label>
+          <input
+            className='border-[1px] border-white   bg-gray-300 p-2 rounded-md'
+            placeholder='Enter Rating'
+            required
+            name='rating'
+            type='text'
+          />
+        </div>
+
+        <div className='flex flex-col gap-1'>
+          <label className='font-semibold'>
+            Processing Time <span className='text-red-500'>&#42;</span>
+          </label>
+          <input
+            className='border-[1px] border-white   bg-gray-300 p-2 rounded-md'
+            placeholder='Enter Processing Time'
+            required
+            name='processing_time'
+            type='text'
+          />
+        </div>
+
+        <div className='flex flex-row gap-1  items-center'>
+          <label className='font-semibold'>
+            Customization <span className='text-red-500'>&#42;</span>
+          </label>
+
+          <label>
+            <input
+              type='radio'
+              value='yes'
+              className='input-radio mx-2'
+              name='customization'
+            />
+            Yes
+          </label>
+          <label className='ml-3'>
+            <input
+              type='radio'
+              value='no'
+              className='input-radio mx-2'
+              name='customization'
+            />
+            No
+          </label>
+        </div>
+
+        <div className='flex flex-row gap-1  items-center'>
+          <label className='font-semibold'>
+            Stock Status <span className='text-red-500'>&#42;</span>
+          </label>
+
+          <label>
+            <input
+              type='radio'
+              value='madeToOrder'
+              className='input-radio mx-2'
+              name='stockStatus'
+            />
+            Made to Order
+          </label>
+          <label className='ml-3'>
+            <input
+              type='radio'
+              value='inStock'
+              className='input-radio mx-2'
+              name='stockStatus'
+            />
+            In Stock
+          </label>
+        </div>
+
+        <div className='flex flex-col gap-1 col-span-1 md:col-span-2'>
+          <label className='font-semibold'>
+            Short Description <span className='text-red-500'>&#42;</span>
+          </label>
+          <input
+            className='border-[1px] border-white   bg-gray-300 p-2 rounded-md'
+            placeholder='Enter Description'
+            required
+            name='short_description'
+            type='text'
+          />
+        </div>
+        <div></div>
+
+        <div className='col-span-1 md:col-span-2'>
+          <input
+            className='btn btn-primary w-full '
+            type='submit'
+            value='Submit'
+          />
         </div>
       </form>
     </div>
